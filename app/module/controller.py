27f5772d0ce0d6016376b5b4cee27beea2885d1a
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-03-31 13:33:52 -0600

    Updated index page

--- app/module/controller.py
+++ app/module/controller.py
@@ -3,10 +3,40 @@ from app import app
 from .models import *
 from .const import HttpStatus
 
+## <-- home/index --> ###
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "<h1>Hola</h1>"
+    indexBody = """
+<h1>Bienvenidx a la API abierta de las elecciones de México 2021</h1>
+</br>
+Para conocer más acerca del proyecto, te recomendamos visitar el repositorio de GitHhub o nuestras redes sociales.
+</br>
+<ul>
+    <li><a href="https://github.com/SocialTIC/mx-elections-2021">Repositorio de GitHub del proyecto</a></li>
+    <li><a href="https://socialtic.org">Sitio de SocialTIC</a></li>
+    <li><a href="https://www.facebook.com/Socialtic/">Facebook de SocialTIC</a></li>
+    <li><a href="https://twitter.com/socialtic/">Twitter de SocialTIC</a></li>
+    <li><a href="https://www.instagram.com/socialtic/">Instagram de SocialTIC</a></li>
+</ul>
+<pre>
+           __________
+         .'----------`.
+         | .--------. |
+         | |########| |
+         | |########| |      /__________\\
+.--------| `--------' |------|    --=-- |-------------.
+|        `----,-.-----'      |o ======  |             |
+|       ______|_|_______     |__________|             |
+|      /  %%%%%%%%%%%%  \                             |
+|     /  %%%%%%%%%%%%%%  \                            |
+|     ^^^^^^^^^^^^^^^^^^^^                            |
++-----------------------------------------------------+
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+</pre>
+    """
+    return indexBody
 
+## <-- person --> ###
 @app.route('/person', methods=['GET', 'POST'])
 def person():
     if request.method == 'GET':
