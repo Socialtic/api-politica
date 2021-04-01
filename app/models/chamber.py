318de9de91b3ba98ec9ef0d4a368e7979bf8309d
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-03-31 23:14:24 -0600

    Created model for table chamber

---
+++ app/models/chamber.py
@@ -0,0 +1,34 @@
+from app import db
+
+class Chamber(db.Model):
+    __tablename__ = 'chamber'
+    __table_args__ = {'sqlite_autoincrement': True}
+
+    chamber_id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    area_id = db.Column(db.Integer, db.ForeignKey('area.area_id'), nullable=True)
+
+    def __init__(self, name, area_id):
+        self.name = name
+        self.area_id = area_id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def getAll():
+        chambers = Chamber.query.all()
+        result = []
+        for chamber in chambers:
+            obj = {
+                'chamber_id': chamber.chamber_id,
+                'name': chamber.name,
+                'area_id': chamber.area_id
+            }
+            result.append(obj)
+        return result
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
