2c18e02958b106bfb4d622c07deed7ab60fff5b4
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-04-09 12:28:40 -0500

    Fixed const

--- app/const.py
+++ app/const.py
@@ -14,6 +14,35 @@ class OtherNames:
     NICKNAME = 2
     BALLOT_NAME = 3
 
+class URL_OWNER_TYPE:
+    PERSON = 1
+    PARTY = 2
+    COALITION = 3
+    MEMBERSHIP = 4
+
+class URL_TYPE:
+    WEBSITE_CAMPAIGN = 1
+    WEBSITE_OFFICIAL = 2
+    WEBSITE_PERSONAL = 3
+    FACEBOOK_CAMPAIGN = 4
+    FACEBOOK_OFFICIAL = 5
+    FACEBOOK_PERSONAL = 6
+    INSTAGRAM_CAMPAIGN = 7
+    INSTAGRAM_OFFICIAL = 8
+    INSTAGRAM_PERSONAL = 9
+    WHATSAPP = 10
+    TWITTER = 11
+    YOUTUBE = 12
+    LINKEDIN = 13
+    FLICKR = 14
+    PINTEREST = 15
+    TUMBLR = 16
+    RSS = 17
+    EMAIL = 18
+    PHOTO = 19
+    LOGO = 20
+    SOURCE_OF_TRUTH = 21
+
 class Catalogues:
     DEGREES_OF_STUDIES = ['', 'ELEMENTARY', 'HIGH SCHOOL', 'ASSOCIATE DEGREE', 'BACHELOR’S DEGREE', 'UNIVERSITY 1 ST PROFESSIONAL DEGREE', 'MASTER DEGREE', 'PHD DEGREE']
     #   GOBERNADOR, DIPUTADO , PRESIDENTE MUNICIPAL
@@ -22,6 +51,5 @@ class Catalogues:
     MEMBERSHIP_TYPES = ['', 'officeholder', 'campaigning_politician', 'party_leader']
     # privilegiado, apodo, nombre de la boleta
     OTHER_NAMES_TYPES = ['', 'preferred', 'nickname', 'ballot_name']
-    PROFESSIONS = ['', 'INGENIEROS', 'ABOGADOS', 'MÉDICOS', 'ARQUITECTO', 'PERIODISTA', 'VETERINARIO', 'DENTISTA']
     #   GOBERNADOR, DIPUTADO , PRESIDENTE MUNICIPAL
-    ROLE_TYPES = ['', 'governmentOfficer', 'legislatorUpperBody', 'executiveCouncil']
+    ROLE_TYPES = ['', 'governmentOfficer', 'legislatorLowerBody', 'executiveCouncil']
