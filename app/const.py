dc25d62f6a62b179299e0210ee993f9fcfe80e05
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-05-04 19:56:35 -0500

    Fixed url name types.

--- app/const.py
+++ app/const.py
@@ -64,8 +64,9 @@ class Catalogues:
                       'WhatsApp', 'Twitter', 'YouTube', 'LinkedIn', 'Flickr', 'Pinterest', 'Tumblr', 'RSS',
                       ]
     URL_TYPE_FULL_NAMES = ['',
-                      'Website campaign', 'Website official', 'Website personal', 'Website Wikipedia',
-                      'Facebook campaign', 'Facebook official', 'Facebook personal',
-                      'Instagram campaign', 'Instagram official', 'Instagram personal',
-                      'WhatsApp', 'Twitter', 'YouTube', 'LinkedIn', 'Flickr', 'Pinterest', 'Tumblr', 'RSS',
+                           'Website campaign', 'Website official', 'Website personal', 'Website Wikipedia',
+                           'Facebook campaign', 'Facebook official', 'Facebook personal',
+                           'Instagram campaign', 'Instagram official', 'Instagram personal',
+                           'WhatsApp', 'Twitter', 'YouTube', 'LinkedIn', 'Flickr', 'Pinterest', 'Tumblr', 'RSS',
+                           'Email', 'Photo', 'Logo', 'source_of_truth'
                       ]
