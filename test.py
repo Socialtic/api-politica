cade9aa2e2d3ac7f431fb5f1dba0e94a7ef5e95f
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-05-05 21:00:28 -0500

    Changed coalition endpoint to swagger model.

--- test.py
+++ test.py
@@ -127,7 +127,7 @@ data = [
 
 for i in range(len(data)):
     print(data[i])
-    response = requests.post(BASE + 'coalition', json=data[i])
+    response = requests.post(BASE + 'coalition/', json=data[i])
     print(response.json())
 print('-----------------------------------------------------------------------')
 print()
