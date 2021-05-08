2d9c20749d9fe00d06a099b65e8c91762ffa40a1
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-05-07 20:24:49 -0500

    Added page for token.

--- app/controllers/token.py
+++ app/controllers/token.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template
 from flask import request
 
-bp_frontend = Blueprint('token', __name__, url_prefix='/')
+bp_token = Blueprint('token', __name__, url_prefix='/')
 
-@bp_frontend.route('/token')
+@bp_token.route('/token/')
 def token():
     id_token = request.args.get('id_token')
     access_token = request.args.get('access_token')
-    return render_template('token.html', access_token = access_token, id_token = id_token)
+    return render_template('token.html', access_token=access_token, id_token=id_token)
