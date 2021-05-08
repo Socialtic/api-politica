09a86f997d82c3a674bf0b9b667115fbb55f26c3
xuxoramos <ramos.cardona@gmail.com>
2021-05-07 22:02:40 -0500

    Updated token page and changed auth method

--- app/controllers/token.py
+++ app/controllers/token.py
@@ -1,10 +1,19 @@
 from flask import Blueprint, render_template
 from flask import request
+import requests
 
 bp_token = Blueprint('token', __name__, url_prefix='/')
 
 @bp_token.route('/token/')
 def token():
-    id_token = request.args.get('id_token')
-    access_token = request.args.get('access_token')
-    return render_template('token.html', access_token=access_token, id_token=id_token)
+
+    url = 'https://mx-elections-2021-api.auth.us-east-2.amazoncognito.com'
+    authorization_code = request.args.get('code')
+    client_id = '6db6blllaim1ebqnefoe61bn9l'
+    grant_type = 'authorization_code'
+
+
+    response = requests.post(url + '/oauth2/token',{'Content-Type':'application/x-www-form-urlencoded', 'grant_type': grant_type, 'client_id': client_id, 'code': authorization_code, 'redirect_uri': 'https://www.candidaturas.mx/token'})
+
+
+    return render_template('token.html', response_json = response.json())
