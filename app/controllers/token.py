0da1a173af87a669b6acbcdb0a4a6885b8f85a52
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-05-11 18:23:45 -0500

    Added error page and endpoints on token view.

--- app/controllers/token.py
+++ app/controllers/token.py
@@ -5,25 +5,28 @@ from json2html import *
 
 bp_token = Blueprint('token', __name__, url_prefix='/')
 
+
 @bp_token.route('/token/')
 def token():
+    try:
+        url = 'https://mx-elections-2021-api.auth.us-east-2.amazoncognito.com/oauth2/token'
+        authorization_code = request.args.get('code')
+        client_id = '6db6blllaim1ebqnefoe61bn9l'
+        grant_type = 'authorization_code'
+        authorization = 'Basic NmRiNmJsbGxhaW0xZWJxbmVmb2U2MWJuOWw6MWljMWFlcXBkNTRvcTB1cXBuOG1uc2xkNzc4MmpmZ240cW84OGVwdWtmMmptYjI1YmU5ZA=='
 
-    url = 'https://mx-elections-2021-api.auth.us-east-2.amazoncognito.com/oauth2/token'
-    authorization_code = request.args.get('code')
-    client_id = '6db6blllaim1ebqnefoe61bn9l'
-    grant_type = 'authorization_code'
-    authorization = 'Basic NmRiNmJsbGxhaW0xZWJxbmVmb2U2MWJuOWw6MWljMWFlcXBkNTRvcTB1cXBuOG1uc2xkNzc4MmpmZ240cW84OGVwdWtmMmptYjI1YmU5ZA=='
+        headers = {
+            'Content-Type': "application/x-www-form-urlencoded",
+            'Authorization': authorization,
+        }
 
-    headers = {
-        'Content-Type': "application/x-www-form-urlencoded",
-        'Authorization': authorization,
-    }
+        body = 'grant_type=' + grant_type + '&client_id=' + client_id + '&code=' + authorization_code + '&redirect_uri=https://www.apielectoral.mx/token'
 
-    body='grant_type=' + grant_type + '&client_id=' + client_id + '&code=' + authorization_code + '&redirect_uri=https://www.apielectoral.mx/token'
+        response = requests.request('POST', url, headers=headers, data=body)
+        # print(response.request.url)
+        # print(response.request.body)
+        # print(response.request.headers)
 
-    response = requests.request('POST', url, headers=headers, data=body)
-    #print(response.request.url)
-    #print(response.request.body)
-    #print(response.request.headers)
-
-    return render_template('token.html', authorization_code=authorization_code, response=json2html.convert(json=response.json()))
+        return render_template('token.html', authorization_code=authorization_code, response=json2html.convert(json=response.json()))
+    except:
+        return render_template('error.html')
