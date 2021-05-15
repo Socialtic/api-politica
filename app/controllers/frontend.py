53d3a19593868022b14170413ecfab8e7383c35f
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-05-14 21:20:17 -0500

    Added changelog.

--- app/controllers/frontend.py
+++ app/controllers/frontend.py
@@ -5,3 +5,7 @@ bp_frontend = Blueprint('index', __name__, url_prefix='/')
 @bp_frontend.route('/')
 def index():
     return render_template('index.html')
+
+@bp_frontend.route('/changelog/')
+def changelog():
+    return render_template('changelog.html')
