2e1d7c71cee0521b643864a63d1491f7092ba1ed
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-11-03 18:31:21 -0600

    Updated token data.

--- app/controllers/token.py
+++ app/controllers/token.py
@@ -9,18 +9,18 @@ bp_token = Blueprint('token', __name__, url_prefix='/')
 @bp_token.route('/token/')
 def token():
     try:
-        url = 'https://mx-elections-2021-api.auth.us-east-2.amazoncognito.com/oauth2/token'
+        url = 'https://ar-elections-2021-api.auth.us-east-2.amazoncognito.com/oauth2/token'
         authorization_code = request.args.get('code')
-        client_id = '6db6blllaim1ebqnefoe61bn9l'
+        client_id = '26mcuibmjcn677ikq9b1endmj1'
         grant_type = 'authorization_code'
-        authorization = 'Basic NmRiNmJsbGxhaW0xZWJxbmVmb2U2MWJuOWw6MWljMWFlcXBkNTRvcTB1cXBuOG1uc2xkNzc4MmpmZ240cW84OGVwdWtmMmptYjI1YmU5ZA=='
+        authorization = 'Basic MjZtY3VpYm1qY242Nzdpa3E5YjFlbmRtajEgMXMxN2hncDFkcXBhcHF1c2Q4NGo2N2xldTU1ajMybTkxdXBlYm1iMTRkc2ttZDlxOWdzOQ=='
 
         headers = {
             'Content-Type': "application/x-www-form-urlencoded",
             'Authorization': authorization,
         }
 
-        body = 'grant_type=' + grant_type + '&client_id=' + client_id + '&code=' + authorization_code + '&redirect_uri=https://www.apielectoral.mx/token/'
+        body = 'grant_type=' + grant_type + '&client_id=' + client_id + '&code=' + authorization_code + '&redirect_uri=https://ar.apielectoral.org/token/'
 
         response = requests.request('POST', url, headers=headers, data=body)
         # print(response.request.url)
@@ -28,5 +28,5 @@ def token():
         # print(response.request.headers)
 
         return render_template('token.html', authorization_code=authorization_code, response=json2html.convert(json=response.json()))
-    except:
+    excempt:
         return render_template('error.html')
