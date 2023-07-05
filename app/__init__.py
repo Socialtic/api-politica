a8277b33c742ecc66d8cd99f5ac1347cef003df0
Penserbjorne <paul.aguilar.enriquez@hotmail.com>
2023-07-05 13:59:33 -0600

    Changed to a new flask context model

--- app/__init__.py
+++ app/__init__.py
@@ -10,19 +10,19 @@ from app.controllers.token import *
 #   Configure for local test
 #   This works with SQLite3
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir, "db/database.db"))
+#database_file = "sqlite:///{}".format(os.path.join(project_dir, "db/database_campaign.db"))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "db/database_officeholders.db"))
 
 #   Configure for RDS/AWS MySQL
 #   format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 #database_file = 'mysql+pymysql://user:password@db_identifier.aws_zone.amazonaws.com:3306/db_name'
-
 
 #   Config app
 application = Flask(__name__)
 application.config["SQLALCHEMY_DATABASE_URI"] = database_file
 application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 application.config['JSON_AS_ASCII'] = False
-isOnDev = False
+isOnDev = True
 application.debug = isOnDev
 application.register_blueprint(bp_frontend)
 application.register_blueprint(bp_token)
@@ -30,17 +30,19 @@ CORS(application)
 ma = Marshmallow(application)
 db = SQLAlchemy(application)
 api = Api(application, doc='/docs',
-          title='COL Elections 2022',
-          description='API of information on the Colombian elections of 2022',
+          title='MX Elections 2021',
+          description='API of information on the Mexican elections of 2021',
           version='1.1')
 
 #   Some kind of internal token
 from app.models.token_auth import *
 
-try:
-    INTERNAL_TOKEN = TokenAuth.find_by_id(1)
-except:
-    INTERNAL_TOKEN = True
+application.app_context().push
+with application.app_context():
+	try:
+	    INTERNAL_TOKEN = TokenAuth.find_by_id(1)
+	except:
+	    INTERNAL_TOKEN = True
 
 #   Functions for the app
 from app.controllers.area import *
